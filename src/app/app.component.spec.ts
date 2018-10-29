22d250bd76bf8ba5e75c305f034596b0fef69767
PerJerZ <perjerz3434@gmail.com>
2018-10-30 02:24:10 +0700

    fix all test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'continuous-everything'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('continuous-everything');
+    expect(app.title).toEqual('continuous-everything!');
   });
 
   it('should render title in a h1 tag', () => {
